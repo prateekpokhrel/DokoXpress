e8e579ab9e7cad538c2c6e012f54fcbe5ea320fe
prateekpokhrel <prateekpokhrel000@gmail.com>
2026-07-06 20:39:50 +0530

    Refactor JwtUtil to use externalized JWT secret configuration

--- backend/src/main/java/com/dokoxpress/backend/config/JwtUtil.java
+++ backend/src/main/java/com/dokoxpress/backend/config/JwtUtil.java
@@ -1,22 +1,25 @@
 package com.dokoxpress.backend.config;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "sdnfaosnfoangosndfoisndfionsdofinsoidfnsidofnisdfn"; // 256-bit required
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     private Key getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
